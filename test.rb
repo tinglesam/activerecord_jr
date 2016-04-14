f92f7365cb65e86f0868b131b79842cd90ab7c82
tinglesam <samueltingle@gmail.com>
2016-04-14 11:32:28 +0800

    user can create a short version of their long urls

---
+++ test.rb
@@ -0,0 +1,20 @@
+require_relative 'app'
+
+#run your driver codes here
+
+#cohort tests 
+##test1 = Cohort.where('name = ?', 'Alpha').first
+#p test1
+
+#cohort2 = Cohort.create()
+
+Cohort.new(id: 98, name: "Cohort 4")
+# Cohort.save
+# alltest_s = Student.all
+# alltest_c = Cohort.all
+# p alltest_c
+# #student tests
+# find_test = Cohort.find(1)
+# Cohort.pself
+
+
